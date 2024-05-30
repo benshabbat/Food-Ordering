a8a322a9bf67a9d3b70d0015be5bbb1b287f502b
benshabbat <benshabbat27@gmail.com>
2024-05-30 19:43:28 +0300

    import mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+
+mongoose.connect();
 
 const app = express();
 app.use(express.json());
