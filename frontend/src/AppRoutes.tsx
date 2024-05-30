dcd7775b28fafd70383f7ab9ed69beef613a7054
benshabbat <benshabbat27@gmail.com>
2024-05-30 20:59:57 +0300

    start pages user and home and using to app routes

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,13 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import User from "./pages/user/User";
 
-const AppRoutes = () => {
+function AppRoutes() {
   return (
     <Routes>
-        <Route path="/" element={}/>
-        <Route path="/user-profile" element={}/>
-        <Route path="*" element={<Navigate to="/"/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/user-profile" element={<User />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  )
+  );
 }
 
-export default AppRoutes
+export default AppRoutes;
