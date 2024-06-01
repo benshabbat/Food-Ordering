85d29dbff3e0edc4f6bd2c028ca5d9cb4bbdb2c3
benshabbat <benshabbat27@gmail.com>
2024-06-01 23:27:54 +0300

    add layout

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import User from "./pages/user/User";
+import Layout from "./layouts/Layout";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Layout>Home</Layout>} />
       <Route path="/user-profile" element={<User />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
