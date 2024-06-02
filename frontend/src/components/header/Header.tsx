be4f998ec83fcdede9ed5b3d8996d1e385faa012
benshabbat <benshabbat27@gmail.com>
2024-06-02 23:03:37 +0300

    logo

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="border-b-2 border-b-orange-500 py-6">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">Logo</Link>
+        <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500"><img src="" alt="logo" /></Link>
       </div>
     </div>
   )
