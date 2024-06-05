9c2192ab7e72165fd794a52ac2f7158e7a78d0f1
benshabbat <benshabbat27@gmail.com>
2024-06-05 17:25:12 +0300

    Menu lucide react

--- frontend/src/components/mobileNav/MobileNav.tsx
+++ frontend/src/components/mobileNav/MobileNav.tsx
@@ -1,10 +1,11 @@
+import { Menu } from "lucide-react"
 import { Sheet, SheetTrigger } from "../ui/sheet"
 
 const MobileNav = () => {
   return (
     <Sheet>
         <SheetTrigger>
-            
+            <Menu className="text-orange-500"/>
         </SheetTrigger>
     </Sheet>
   )
