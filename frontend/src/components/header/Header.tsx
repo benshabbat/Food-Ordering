d5c438969716af415734bec7fe6ebb15cb7e3503
benshabbat <benshabbat27@gmail.com>
2024-06-05 17:56:18 +0300

    MobileNav to header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
+import MobileNav from "../mobileNav/MobileNav"
 
 const Header = () => {
   return (
     <div className="border-b-2 border-b-orange-500 py-6">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500"><img src="" alt="logo" /></Link>
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
       </div>
     </div>
   )
--- frontend/src/components/mobileNav/MobileNav.tsx
+++ frontend/src/components/mobileNav/MobileNav.tsx
@@ -1,19 +1,33 @@
-import { Menu } from "lucide-react"
-import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "../ui/sheet"
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
 
 const MobileNav = () => {
   return (
     <Sheet>
-        <SheetTrigger>
-            <Menu className="text-orange-500"/>
-        </SheetTrigger>
-        <SheetContent>
-            <SheetTitle>
-                <span>Welcom to my Ordering Food</span>
-            </SheetTitle>
-        </SheetContent>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle>
+          <span>Welcom to my Ordering Food</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+            <Button className="flex-1 font-bold bg-orange-500">
+                Log In
+            </Button>
+        </SheetDescription>
+      </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
-export default MobileNav
+export default MobileNav;
